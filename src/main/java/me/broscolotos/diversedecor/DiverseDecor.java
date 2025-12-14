6cb42899dd1ebd36bc16b4a2df07b3c264c0e30c
broscolotos <broscolotos@gmail.com>
2025-12-13 21:38:42 -0600

    Version bump

--- src/main/java/me/broscolotos/diversedecor/DiverseDecor.java
+++ src/main/java/me/broscolotos/diversedecor/DiverseDecor.java
@@ -29,7 +29,7 @@ public class DiverseDecor {
 
     public static final String MODID = "diversedecor";
     public static final String NAME = "DiverseDecor";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "0.3.2";
     public static CreativeTabs diverseDecorCreativeTab, diverseDecorBlockTab, diverseDecorSubBlocksTab, diverseDecorCosmeticsTab;
     public static Logger blockLogger = LogManager.getLogger("DiverseDecor");
 
